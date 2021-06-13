17fec014edb6713c7035367377c08dd4bcfad66b
anthugny <62931219+anthugny@users.noreply.github.com>
2021-06-13 23:55:18 +0100

    hardcoded the submit functionality
    It appears the submit button is not working as expected.

--- src/routes/sites_routes.js
+++ src/routes/sites_routes.js
@@ -1,7 +1,7 @@
-const router = require('express').Router();
-const { param, query } = require('express-validator');
-const apiErrorReporter = require('../utils/apierrorreporter');
-const controller = require('../controllers/sites_controller.js');
+const router = require("express").Router();
+const { param, query } = require("express-validator");
+const apiErrorReporter = require("../utils/apierrorreporter");
+const controller = require("../controllers/sites_controller.js");
 
 /**
  * Custom validate.js validator.  Validates a set of parameters to
@@ -13,76 +13,75 @@ const controller = require('../controllers/sites_controller.js');
  * @private
  */
 const geoParamsValidator = (value, { req }) => {
-  const {
-    lat, lng, radius, radiusUnit,
-  } = req.query;
+  const { lat, lng, radius, radiusUnit } = req.query;
 
   if (lat && lng && radius && radiusUnit) {
     return true;
   }
 
-  throw new Error('When using geo lookup, params lat, lng, radius, radiusUnit are required.');
+  throw new Error(
+    "When using geo lookup, params lat, lng, radius, radiusUnit are required."
+  );
 };
 
 // GET /sites
 router.get(
-  '/sites',
+  "/sites",
   [
     /* eslint-disable newline-per-chained-call */
-    query('lat').optional().custom(geoParamsValidator).isFloat().toFloat(),
-    query('lng').optional().custom(geoParamsValidator).isFloat().toFloat(),
-    query('radius').optional().custom(geoParamsValidator).isFloat({ min: 0.1 }).toFloat(),
-    query('radiusUnit').optional().custom(geoParamsValidator).isIn(['MI', 'KM']),
-    query('onlyExcessCapacity').optional().isBoolean().toBoolean(),
+    query("lat").optional().custom(geoParamsValidator).isFloat().toFloat(),
+    query("lng").optional().custom(geoParamsValidator).isFloat().toFloat(),
+    query("radius")
+      .optional()
+      .custom(geoParamsValidator)
+      .isFloat({ min: 0.1 })
+      .toFloat(),
+    query("radiusUnit")
+      .optional()
+      .custom(geoParamsValidator)
+      .isIn(["MI", "KM"]),
+    query("onlyExcessCapacity").optional().isBoolean().toBoolean(),
     /* eslint-enable */
     apiErrorReporter,
   ],
   async (req, res, next) => {
     try {
-      const {
-        lat, lng, radius, radiusUnit, onlyExcessCapacity,
-      } = {
+      const { lat, lng, radius, radiusUnit, onlyExcessCapacity } = {
         lat: 37.8387892,
-        lng:  -122.271111,
+        lng: -122.271111,
         radius: 10,
-        radiusUnit: 'KM'
+        radiusUnit: "KM",
       };
-      
-      
-      const sites = (
-        lat
-          ? await controller.getSitesNearby(
+
+      const sites = lat
+        ? await controller.getSitesNearby(
             lat,
             lng,
             radius,
             radiusUnit,
-            onlyExcessCapacity,
+            onlyExcessCapacity
           )
-          : await controller.getSites()
-      );
+        : await controller.getSites();
 
       return res.status(200).json(sites);
     } catch (err) {
       return next(err);
     }
-  },
+  }
 );
 
 // GET /sites/999
 router.get(
-  '/sites/:siteId',
-  [
-    param('siteId').isInt().toInt(),
-    apiErrorReporter,
-  ],
+  "/sites/:siteId",
+  [param("siteId").isInt().toInt(), apiErrorReporter],
   async (req, res, next) => {
     try {
       const site = await controller.getSite(req.params.siteId);
-      return (site ? res.status(200).json(site) : res.sendStatus(404));
+      return site ? res.status(200).json(site) : res.sendStatus(404);
     } catch (err) {
       return next(err);
     }
-  },
+  }
 );
 
 module.exports = router;
